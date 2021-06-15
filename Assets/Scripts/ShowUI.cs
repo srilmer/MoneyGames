a2f4d0fd41007b56c81d4f3a18a585cf1edefb9b
Jeremy Smits <45685897+JeremySmits@users.noreply.github.com>
2021-06-15 12:20:44 +0200

    Merging all branches together version 0.5

---
+++ Assets/Scripts/ShowUI.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowUI : MonoBehaviour
+{
+    [SerializeField]
+    private Canvas kapper;
+
+    [SerializeField]
+    private Canvas brandweer;
+
+    [SerializeField]
+    private Canvas postbode;
+
+    [SerializeField]
+    private Canvas pizzaBezorger;
+
+    [SerializeField]
+    private Canvas boer;
+
+    [SerializeField]
+    private Canvas tandarts;
+
+    private GameObject ThePlayer;
+
+    private bool inTrigger;
+    private Vector3 startPos;
+
+    void Start()
+    {
+        // Get Player and playermovementscript info
+        ThePlayer = GameObject.FindWithTag("Player");
+        inTrigger = false;
+        startPos = ThePlayer.transform.position;
+    }
+
+    IEnumerator OnTriggerEnter(Collider player)
+    {
+        inTrigger = true;
+        yield return new WaitForSeconds(1);
+
+        // Check if the object has 'player' tag
+        // Check if the object is in the trigger after 2 seconds
+        // Check if the player has moved from it's original location
+        if(player.CompareTag("Player") && inTrigger && (startPos != ThePlayer.transform.position))
+        {
+            switch (this.tag.ToString())
+            {
+                case "Kapper":
+                    kapper.enabled = true;
+                    break;
+                case "Brandweer":
+                    brandweer.enabled = true;
+                    break;
+                case "Postbode":
+                    postbode.enabled = true;
+                    break;
+                case "PizzaBezorger":
+                    pizzaBezorger.enabled = true;
+                    break;
+                case "tandarts":
+                    tandarts.enabled = true;
+                    break;
+                case "Boer":
+                    boer.enabled = true;
+                    break;
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider player)
+    {
+        inTrigger = false;
+    }
+}
